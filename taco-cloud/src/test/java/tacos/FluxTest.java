0cdcba768b04db7aa8eefb4ae7af4b13b8333ea2
Rungrukuk <rungrukuk@gmail.com>
2024-07-19 17:14:34 +0400

    Chapter 11.3.3 learned new flux functions

--- taco-cloud/src/test/java/tacos/FluxTest.java
+++ taco-cloud/src/test/java/tacos/FluxTest.java
@@ -2,11 +2,17 @@ package tacos;
 
 import java.time.Duration;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Stream;
 
 import org.junit.jupiter.api.Test;
+
+import lombok.Data;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
 import reactor.test.StepVerifier;
 import reactor.util.function.Tuple2;
 
@@ -176,6 +182,169 @@ public class FluxTest {
                                 .expectNext("tortoise")
                                 .expectNext("snail")
                                 .expectNext("sloth")
+                                .verifyComplete();
+        }
+
+        @Test
+        public void skipAFew() {
+                Flux<String> countFlux = Flux.just(
+                                "one", "two", "skip a few", "ninety nine", "one hundred")
+                                .skip(3);
+                StepVerifier.create(countFlux)
+                                .expectNext("ninety nine", "one hundred")
+                                .verifyComplete();
+        }
+
+        @Test
+        public void skipAFewSeconds() {
+                Flux<String> countFlux = Flux.just(
+                                "one", "two", "skip a few", "ninety nine", "one hundred")
+                                .delayElements(Duration.ofSeconds(1))
+                                .skip(Duration.ofSeconds(4));
+                StepVerifier.create(countFlux)
+                                .expectNext("ninety nine", "one hundred")
+                                .verifyComplete();
+        }
+
+        @Test
+        public void take() {
+                Flux<String> nationalParkFlux = Flux.just(
+                                "Yellowstone", "Yosemite", "Grand Canyon", "Zion", "Acadia")
+                                .take(3);
+                StepVerifier.create(nationalParkFlux)
+                                .expectNext("Yellowstone", "Yosemite", "Grand Canyon")
+                                .verifyComplete();
+        }
+
+        @Test
+        public void takeForAwhile() {
+                Flux<String> nationalParkFlux = Flux.just(
+                                "Yellowstone", "Yosemite", "Grand Canyon", "Zion", "Grand Teton")
+                                .delayElements(Duration.ofSeconds(1))
+                                .take(Duration.ofMillis(3500));
+                StepVerifier.create(nationalParkFlux)
+                                .expectNext("Yellowstone", "Yosemite", "Grand Canyon")
+                                .verifyComplete();
+        }
+
+        @Test
+        public void filter() {
+                Flux<String> nationalParkFlux = Flux.just(
+                                "Yellowstone", "Yosemite", "Grand Canyon", "Zion", "Grand Teton")
+                                .filter(np -> !np.contains(" "));
+                StepVerifier.create(nationalParkFlux)
+                                .expectNext("Yellowstone", "Yosemite", "Zion")
+                                .verifyComplete();
+        }
+
+        @Test
+        public void distinct() {
+                Flux<String> animalFlux = Flux.just(
+                                "dog", "cat", "bird", "dog", "bird", "anteater")
+                                .distinct();
+                StepVerifier.create(animalFlux)
+                                .expectNext("dog", "cat", "bird", "anteater")
+                                .verifyComplete();
+        }
+
+        @Test // synchronous
+        public void map() {
+                Flux<Player> playerFlux = Flux
+                                .just("Michael Jordan", "Scottie Pippen", "Steve Kerr")
+                                .map(n -> {
+                                        String[] split = n.split("\\s");
+                                        return new Player(split[0], split[1]);
+                                });
+
+                StepVerifier.create(playerFlux)
+                                .expectNext(new Player("Michael", "Jordan"))
+                                .expectNext(new Player("Scottie", "Pippen"))
+                                .expectNext(new Player("Steve", "Kerr"))
+                                .verifyComplete();
+        }
+
+        @Test // asynchronous
+        public void flatMap() {
+                Flux<Player> playerFlux = Flux
+                                .just("Michael Jordan", "Scottie Pippen", "Steve Kerr").flatMap(n -> Mono.just(n)
+                                                .map(p -> {
+                                                        String[] split = p.split("\\s");
+                                                        return new Player(split[0], split[1]);
+                                                })
+                                                .subscribeOn(Schedulers.parallel()));
+
+                List<Player> playerList = Arrays.asList(
+                                new Player("Michael", "Jordan"),
+                                new Player("Scottie", "Pippen"),
+                                new Player("Steve", "Kerr"));
+                StepVerifier.create(playerFlux)
+                                .expectNextMatches(p -> playerList.contains(p))
+                                .expectNextMatches(p -> playerList.contains(p))
+                                .expectNextMatches(p -> playerList.contains(p))
+                                .verifyComplete();
+        }
+
+        @Data
+        private static class Player {
+                private final String firstName;
+                private final String lastName;
+        }
+
+        @Test
+        public void buffer() {
+                Flux<String> fruitFlux = Flux.just(
+                                "apple", "orange", "banana", "kiwi", "strawberry");
+
+                Flux<List<String>> bufferedFlux = fruitFlux.buffer(3);
+
+                StepVerifier
+                                .create(bufferedFlux)
+                                .expectNext(Arrays.asList("apple", "orange", "banana"))
+                                .expectNext(Arrays.asList("kiwi", "strawberry"))
+                                .verifyComplete();
+        }
+
+        @Test
+        public void bufferAndFlatMap() throws Exception {
+                Flux.just(
+                                "apple", "orange", "banana", "kiwi", "strawberry")
+                                .buffer(3)
+                                .flatMap(x -> Flux.fromIterable(x)
+                                                .map(y -> y.toUpperCase())
+                                                .subscribeOn(Schedulers.parallel())
+                                                .log())
+                                .subscribe();
+        }
+
+        @Test
+        public void collectList() {
+                Flux<String> fruitFlux = Flux.just(
+                                "apple", "orange", "banana", "kiwi", "strawberry");
+
+                Mono<List<String>> fruitListMono = fruitFlux.collectList();
+
+                StepVerifier
+                                .create(fruitListMono)
+                                .expectNext(Arrays.asList(
+                                                "apple", "orange", "banana", "kiwi", "strawberry"))
+                                .verifyComplete();
+        }
+
+        @Test
+        public void collectMap() {
+                Flux<String> animalFlux = Flux.just(
+                                "aardvark", "elephant", "koala", "eagle", "kangaroo");
+
+                Mono<Map<Character, String>> animalMapMono = animalFlux.collectMap(a -> a.charAt(0));
+
+                StepVerifier
+                                .create(animalMapMono)
+                                .expectNextMatches(map -> {
+                                        return map.size() == 3 &&
+                                                        map.get('a').equals("aardvark") &&
+                                                        map.get('e').equals("eagle") &&
+                                                        map.get('k').equals("kangaroo");
+                                })
                                 .verifyComplete();
         }
 
